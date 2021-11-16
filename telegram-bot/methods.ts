20af57c3d19be2c31efb58659ecfa8ccfa54612e
Arthur G <lapteuh@gmail.com>
2021-11-16 21:54:13 +0500

    Extract methods to new file

--- telegram-bot/methods.ts
+++ telegram-bot/methods.ts
@@ -1,16 +1,44 @@
 import axios from "axios";
-import { getBotToken } from "./config";
+import { getBotToken, getBotWebhookURL } from "./config";
+import { Response } from "./types/Response";
+
+type WebhookInfo = {
+  url: string;
+};
 
 export async function sendMessage(params: {
   chatId: number;
   text: string;
   replyToMessageId?: number;
 }): Promise<void> {
-  await axios.post(`https://api.telegram.org/bot${getBotToken()}/sendMessage`, {
+  await axios.post<Response<WebhookInfo>>(method("sendMessage"), {
     chat_id: params.chatId,
     text: params.text,
     ...(params.replyToMessageId
       ? { reply_to_message_id: params.replyToMessageId }
       : {}),
   });
+}
+
+export async function getWebhookInfo(): Promise<string> {
+  const resp = await axios.post<Response<WebhookInfo>>(
+    method("getWebhookInfo"),
+    {}
+  );
+
+  if (!resp.data.ok) {
+    throw new Error(resp.data.description);
+  }
+
+  return resp.data.result.url;
+}
+
+export async function setWebhook(): Promise<void> {
+  await axios.post<Response<WebhookInfo>>(method("setWebhook"), {
+    url: getBotWebhookURL(),
+  });
+}
+
+function method(name: "getWebhookInfo" | "sendMessage" | "setWebhook") {
+  return `https://api.telegram.org/bot${getBotToken()}/${name}` as const;
 }
