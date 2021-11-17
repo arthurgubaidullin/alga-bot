8f56128d40d5d12863d4419945296a90e885702f
Arthur G <lapteuh@gmail.com>
2021-11-17 20:53:29 +0500

    Add bot handler

--- pages/api/bot/[token].ts
+++ pages/api/bot/[token].ts
@@ -1,8 +1,10 @@
-import { cert, initializeApp, getApp, getApps, App } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
+import { Axios, AxiosError } from "axios";
+import { App, cert, initializeApp } from "firebase-admin/app";
+import { FieldValue, Firestore, getFirestore } from "firebase-admin/firestore";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { isSpam } from "../../../spam-detector";
 import { getBotToken } from "../../../telegram-bot/config";
-import { sendMessage } from "../../../telegram-bot/methods";
+import { banChatMember, sendMessage } from "../../../telegram-bot/methods";
 import { Update } from "../../../telegram-bot/types/Update";
 
 let firebaseApp: App | null = null;
@@ -26,31 +28,59 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query?.token && req.query.token !== getBotToken()) {
-    res.status(403).end();
-    return;
-  }
+  try {
+    if (req.query?.token && req.query.token !== getBotToken()) {
+      res.status(403).end();
+      return;
+    }
 
-  const update = req.body as Update | undefined;
-
-  if (typeof update === "string") {
-    console.log("oops!");
-    res.status(200).end();
-    return;
-  }
-
-  console.log(update);
-
-  {
     const app = getFirebaseApp();
     const db = getFirestore(app);
-    if (update) {
-      await db.collection("telegram_updates").add(update);
-    }
-  }
 
+    const update = req.body as Update;
+
+    console.log("Update", update);
+
+    const findAndBanSpammerTask = findAndBanSpammers(db, update);
+    const logUpdateTask = logUpdateToFirestore(db, update);
+    const echoInPrivateChatTask = echoInPrivateChat(update);
+
+    await Promise.all([
+      logUpdateTask,
+      findAndBanSpammerTask,
+      echoInPrivateChatTask,
+    ]);
+
+    res.status(200).end();
+  } catch (error: unknown) {
+    logError(error as AxiosError | Error);
+
+    // console.error(error);
+    res.status(500).end();
+  }
+}
+
+function logError(error: AxiosError | Error) {
+  if ("response" in error) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    error.response && console.error(error.response.data);
+    error.response && console.error(error.response.status);
+    error.response && console.error(error.response.headers);
+  } else if ("request" in error) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.error(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.error(error.message);
+  }
+  "config" in error && console.error(error.config);
+}
+
+async function echoInPrivateChat(update: Update) {
   if (
-    update &&
     update.message &&
     update.message.chat &&
     update.message.from &&
@@ -58,14 +88,66 @@ export default async function handler(
     !update.message.from.is_bot &&
     update.message.chat.type === "private"
   ) {
-    // send echo
-    console.log("echo");
     await sendMessage({
       chatId: update.message.chat.id,
       text: update.message.text,
       replyToMessageId: update.message.message_id,
     });
   }
+}
 
-  res.status(200).end();
+async function findAndBanSpammers(
+  db: Firestore,
+  update: Update
+): Promise<void> {
+  const message = update.message;
+  const chat = message?.chat;
+  const newChatMembers = message?.new_chat_members;
+  if (
+    newChatMembers &&
+    chat &&
+    (chat.type === "group" || chat.type === "supergroup") &&
+    newChatMembers.length > 0
+  ) {
+    const spammers = newChatMembers
+      .map((m) =>
+        isSpam(m.first_name + (m.last_name || ""))
+          ? {
+              id: m.id,
+              firstName: m.first_name,
+              ...(m.last_name ? { lastName: m.last_name } : {}),
+            }
+          : null
+      )
+      .filter(<T>(spammer: T): spammer is NonNullable<T> => spammer !== null);
+
+    await Promise.all(
+      spammers.map(async (spammer) => {
+        const uid = spammer.id;
+        const ref = db
+          .collection(`telegram_${chat.type}s`)
+          .doc(chat.id.toString())
+          .collection("spammers")
+          .doc(uid.toString());
+
+        const banTask = banChatMember({
+          chatId: chat.id,
+          userId: uid,
+        });
+
+        const createDocTask = ref.create({
+          id: uid.toString(),
+          firstName: spammer.firstName,
+          lastName: spammer.lastName || "",
+          createdAt: FieldValue.serverTimestamp(),
+        });
+
+        await Promise.all([createDocTask, banTask]);
+      })
+    );
+  }
+}
+
+async function logUpdateToFirestore(db: Firestore, update: Update) {
+  await db.collection("telegram_updates").add(update);
 }
