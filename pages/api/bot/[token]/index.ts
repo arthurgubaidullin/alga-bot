b399cc1cdd7c0daae64d51c278585fbcda0006f7
Arthur G <lapteuh@gmail.com>
2021-11-25 03:17:45 +0500

    Refactoring

--- pages/api/bot/[token]/index.ts
+++ pages/api/bot/[token]/index.ts
@@ -1,20 +1,19 @@
 import { AxiosError } from "axios";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type {
+  NextApiRequest,
+  NextApiResponse,
+  NextApiHandler,
+} from "next";
 import { logError } from "../../../../logError";
 import { getBotToken } from "../../../../telegram-bot/config";
 import { Update } from "../../../../telegram-bot/types/Update";
 import * as theBot from "../../../../the-bot";
 
-export default async function handler(
+async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    if (req.query?.token && req.query.token !== getBotToken()) {
-      res.status(403).end();
-      return;
-    }
-
     const update = req.body as Update;
 
     console.log("Update", update);
@@ -28,4 +27,18 @@ export default async function handler(
     // console.error(error);
     res.status(500).end();
   }
+}
+
+export default validateBotToken(handler);
+
+function validateBotToken(
+  handler: NextApiHandler
+): NextApiHandler {
+  return async (req, res) => {
+    if (req.query?.token && req.query.token !== getBotToken()) {
+      res.status(403).end();
+      return;
+    }
+    return handler(req, res);
+  };
 }
