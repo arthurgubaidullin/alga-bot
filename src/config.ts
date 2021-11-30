975495b699752e2826ed0898a9445a1a89fef782
Arthur G <lapteuh@gmail.com>
2021-11-30 19:04:19 +0500

    Add getBotWebhookURL() to config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
-export interface BotConfig {
+export interface GetBotToken {
   getBotToken(): string;
 }
 
 const botToken = process.env.TELEGRAM_BOT_TOKEN;
 
-const botConfig: BotConfig = {
+const getBotToken: GetBotToken = {
   getBotToken(): string {
     if (!botToken) {
       throw new Error(
@@ -15,6 +15,24 @@ const botConfig: BotConfig = {
   },
 };
 
-export type Config = BotConfig;
+export interface GetBotWebhookURL {
+  getBotWebhookURL(): string;
+}
 
-export const config: Config = { ...botConfig };
+const host = process.env.HOST;
+
+export const getBotWebhookURL: GetBotWebhookURL = {
+  getBotWebhookURL() {
+    if (!host) {
+      throw new Error("HOST enviroment variable not setted.");
+    }
+    return `https://${host}/api/bot/${getBotToken.getBotToken()}`;
+  },
+};
+
+export type Config = GetBotToken & GetBotWebhookURL;
+
+export const config: Config = {
+  ...getBotToken,
+  ...getBotWebhookURL,
+};
